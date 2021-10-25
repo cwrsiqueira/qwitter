fc536575d9250c8c1c707e067a81cebba9e9ba47
cwrsiqueira <cwrsiqueira@msn.com>
2021-10-25 16:51:22 -0300

    first commit

---
+++ src/pages/PageAbout.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h4>
+    <div class="text-body1">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta laborum reprehenderit distinctio necessitatibus? Quibusdam unde explicabo non pariatur? Perspiciatis reprehenderit ducimus optio quaerat ad molestias repellat non inventore quam ipsum? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet iste aperiam eveniet repellat. Autem eos iste consequatur fuga aliquam reiciendis dolore non, a aperiam quasi molestias quos dolorum beatae suscipit!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta laborum reprehenderit distinctio necessitatibus? Quibusdam unde explicabo non pariatur? Perspiciatis reprehenderit ducimus optio quaerat ad molestias repellat non inventore quam ipsum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit laudantium similique, saepe minus nemo libero enim doloremque eveniet tenetur architecto nulla vero, illo labore dolores sapiente, iusto error eos neque!</p>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageAbout'
+})
+</script>
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,74 @@
+<template>
+  <q-page>
+
+    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+
+      <div class="col">
+        <q-input
+          class="new-qweet"
+          bottom-slots
+          v-model="newQweetContent"
+          placeholder="What's happening?"
+          counter
+          maxlength="280"
+          autogrow
+        >
+          <template v-slot:before>
+            <q-avatar size="xl">
+              <img src="https://cdn.quasar.dev/img/avatar5.jpg">
+            </q-avatar>
+          </template>
+
+          <template v-slot:append>
+            <q-icon v-if="newQweetContent !== ''" name="close" @click="newQweetContent = ''" class="cursor-pointer" />
+          </template>
+
+        </q-input>
+      </div>
+
+      <div class="col col-shrink">
+        <q-btn
+          :disable="!newQweetContent"
+          class="q-mb-lg"
+          unelevated
+          rounded
+          color="primary"
+          label="Qweet"
+          no-caps
+        />
+      </div>
+
+    </div>
+
+    <q-separator
+      class="divider"
+      size="10px"
+      color="grey-2"
+    />
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageHome',
+  data() {
+    return {
+      newQweetContent: ''
+    }
+  }
+})
+</script>
+
+<style lang="sass">
+.new-qweet
+  textarea
+    font-size: 19px
+    line-height: 1.4 !important
+.divider
+  border-top: 1px solid
+  border-bottom: 1px solid
+  border-color: $grey-4
+</style>
